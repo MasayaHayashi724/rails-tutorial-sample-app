4aba4227da8d24d9c13fe5209cfc2f569c160c4f
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-03-28 02:36:15 +0900

    Modify digest method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   # return hash value for string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
-  
+
 end
