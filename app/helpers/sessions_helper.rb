223b2cb39701b405a106d59064b352ce21a67068
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-03-28 15:53:27 +0900

    Impement remember session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,19 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # remember user in eternal session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # return currently logged in user if any
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  # return true if logged in, false if not
   def logged_in?
     !current_user.nil?
   end
