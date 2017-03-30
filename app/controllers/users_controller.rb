4107045484a218c65c34a72531646d388dfbc0e1
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-03-30 18:39:44 +0900

    Move logged_in_user method to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,16 +49,9 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     # confirm if logged in correct user
