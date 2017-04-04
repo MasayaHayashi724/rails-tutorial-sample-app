583836c353599c4649275f5aac399669646186b8
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-04-04 19:05:01 +0900

    Implement feed method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -93,4 +93,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not tester.following?(archer)
   end
 
+  test "feed should have the right posts" do
+    tester = users(:tester)
+    archer = users(:archer)
+    lana = users(:lana)
+    # confirm following users
+    lana.microposts.each do |post_following|
+      assert tester.feed.include?(post_following)
+    end
+    # confirm my own posts
+    tester.microposts.each do |post_self|
+      assert tester.feed.include?(post_self)
+    end
+    # confirm unfollowing users
+    archer.microposts.each do |post_unfollowed|
+      assert_not tester.feed.include?(post_unfollowed)
+    end
+  end
+
 end
